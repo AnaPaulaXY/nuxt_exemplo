522442037f73a410e76c7a30f042564b7b1b5ad1
unknown <paula.rosa@aluno.ifsp.edu.br>
2022-05-02 16:43:28 -0300

    Criação exemplo Nuxt.js

---
+++ exemplo-front/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Bem-Vindo ao Nuxt.js</h1>
+    <ul>
+      <li>Elemento 1</li>
+      <li>Elemento 2</li>
+      <li>Elemento 3</li>
+
+    </ul>
+  </div>
+<!-- <Tutorial/>-->
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+  h1{
+    color: rgb(255, 2, 150);
+  }
+</style>
---
+++ exemplo-front/pages/lista_pessoas.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>{{titulo}}</h1>
+    <ul>
+        <li :key="item.id" v-for = "item in pessoas">
+            {{item.nome}} - {{item.idade}} anos
+        </li>
+    </ul>
+    <button>Clique aqui</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            titulo: "Lista de pessoas e idades",
+            pessoas: [
+                {id: 1, nome: "Marcio", idade: 18},
+                {id: 2, nome: "Carla", idade: 16},
+                {id: 3, nome: "Lais", idade: 20},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
